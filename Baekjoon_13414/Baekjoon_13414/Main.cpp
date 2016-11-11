adb4778c272f7444d107244c832afddf7fe40566
mar1s <mar1st1145@gmail.com>
2016-11-11 22:28:10 +0900

    commit

--- Baekjoon_13414/Baekjoon_13414/Main.cpp
+++ Baekjoon_13414/Baekjoon_13414/Main.cpp
@@ -1,38 +1,42 @@
-#include <iostream>
-#include <map>
-#include <string>
 #include <vector>
 #include <unordered_map>
+#include <cstdio>
+#include <iostream>
 
 using namespace std;
 
-int N = 0;
-int K = 0;
-int next_size = 0;
-int prev_size = 0;
-string* students;
-unordered_map<string, int> check_list;
-vector<string> result;
-
 int main(void) {
+	int N = 0;
+	int K = 0;
+	unsigned int next_size = 0;
+	unsigned int prev_size = 0;
+	int student = 0;
+	unordered_map<int, int> check_list;
+	vector<int> result;
+	vector<int> students;
+
 	cin >> K >> N;
 
-	students = new string[N];
 	for (int i = 0; i < N; i++) {
-		cin >> students[i];
+		cin >> student;
+		students.push_back(student);
 	}
 
 	for (int i = N - 1; i >= 0; i--) {
 		prev_size = next_size;
-		check_list[students[i]] = 0;
+		check_list[students.at(i)] = 1;
 		next_size = check_list.size();
 		if (prev_size != next_size) {
-			result.push_back(students[i]);
+			result.push_back(students.at(i));
 		}
 	}
 
 	for (int i = 0; i < K; i++) {
-		cout << result.back() << endl;
-		result.pop_back();
+		if (!result.empty()) {
+			printf("%08d\n", result.back());
+			result.pop_back();
+		}
 	}
+
+	return 0;
 }
